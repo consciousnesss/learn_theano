67466c96e4cf8df9a7986d2d2d77fb0aeba3d0b5
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-11-15 22:27:25 -0800

    full example

--- learn_theano/deeplearning_tutorials/test_5_rbm.py
+++ learn_theano/deeplearning_tutorials/test_5_rbm.py
@@ -3,10 +3,12 @@
 import theano
 import theano.tensor as T
 import numpy as np
+from learn_theano.utils.display_filters import tile_raster_images
 from learn_theano.utils.download_all_datasets import get_dataset
-import cPickle
+from theano.tensor.shared_randomstreams import RandomStreams
 import pickle
 import time
+import cv2
 
 
 def one_zero_loss(prediction_labels, labels):
@@ -15,6 +17,10 @@ def one_zero_loss(prediction_labels, labels):
 
 def negative_log_likelihood_loss(prediction_probailities, labels):
     return -T.mean(T.log(prediction_probailities)[T.arange(labels.shape[0]), labels])
+
+
+def mean_cross_entropy(prediction_probailities, labels):
+    return -T.mean(T.sum(labels*T.log(prediction_probailities) + (1-labels)*T.log(1-prediction_probailities), axis=1))
 
 
 def load_dataset(dataset):
@@ -40,30 +46,36 @@ class RBM(object):
         )
 
         self.b_visible = theano.shared(
-            np.zeros((n_hidden,), dtype=theano.config.floatX),
+            np.zeros((n_visible,), dtype=theano.config.floatX),
             name='b_visible',
             borrow=True
         )
 
     def sample_hidden_given_visible(self, visible, theano_rng):
-        hidden_activation = T.nnet.sigmoid(T.dot(visible, self.W)+self.b_hidden)
-        hidden_sample = theano_rng.binominal(
+        hidden_linear_activations = T.dot(visible, self.W)+self.b_hidden
+        hidden_activation = T.nnet.sigmoid(hidden_linear_activations)
+        hidden_sample = theano_rng.binomial(
             size=hidden_activation.shape, n=1, p=hidden_activation, dtype=theano.config.floatX)
-        return hidden_sample
+        return hidden_sample, hidden_activation, hidden_linear_activations
 
     def sample_visible_give_hidden(self, hidden, theano_rng):
-        visible_activation = T.nnet.sigmoid(T.dot(hidden, self.W.T)+self.b_visible)
-        visible_sample = theano_rng.binominal(
+        linear_visible_activations = T.dot(hidden, self.W.T)+self.b_visible
+        visible_activation = T.nnet.sigmoid(linear_visible_activations)
+        visible_sample = theano_rng.binomial(
             size=visible_activation.shape, n=1, p=visible_activation, dtype=theano.config.floatX)
-        return visible_sample
+        return visible_sample, visible_activation, linear_visible_activations
 
     def gibbs_update_hidden_visible_hidden(self, hidden, theano_rng):
-        visible_sample = self.sample_visible_give_hidden(hidden, theano_rng)
-        return visible_sample, self.sample_hidden_given_visible(visible_sample, theano_rng)
+        visible_sample, visible_activation, linear_visible_activation = self.sample_visible_give_hidden(hidden, theano_rng)
+        hidden_sample, hidden_activation, hidden_linear_activation = self.sample_hidden_given_visible(visible_sample, theano_rng)
+        return [visible_sample, visible_activation, linear_visible_activation,
+                hidden_sample, hidden_activation, hidden_linear_activation]
 
     def gibbs_update_visible_hidden_visible(self, visible, theano_rng):
-        hidden_sample = self.sample_hidden_given_visible(visible, theano_rng)
-        return hidden_sample, self.sample_visible_give_hidden(hidden_sample, theano_rng)
+        hidden_sample, hidden_activation, hidden_linear_activation = self.sample_hidden_given_visible(visible, theano_rng)
+        visible_sample, visible_activation, linear_visible_activation = self.sample_visible_give_hidden(hidden_sample, theano_rng)
+        return [hidden_sample, hidden_activation, hidden_linear_activation,
+                visible_sample, visible_activation, linear_visible_activation]
 
     def free_energy(self, visible_sample):
         visible_bias_term = T.dot(visible_sample, self.b_visible)
@@ -77,18 +89,56 @@ class RBM(object):
             chain_start = self.sample_hidden_given_visible(input, theano_rng)
         else:
             chain_start = persistent_state
-        (visible_samples, hidden_samples), updates = theano.scan(
-            fn=self.gibbs_update_hidden_visible_hidden,
-            outputs_info=[None, chain_start],
-            n_steps=number_of_gibbs_steps,
-            non_sequences=theano_rng
+        (visible_samples, visible_activations, linear_visible_activations,
+         hidden_samples, hidden_activations, hidden_linear_activations), updates = theano.scan(
+            fn=lambda x: self.gibbs_update_hidden_visible_hidden(x, theano_rng),
+            outputs_info=[None, None, None, None, None, chain_start],
+            n_steps=number_of_gibbs_steps
         )
         visible_chain_end = visible_samples[-1]
         cost = self.free_energy(input) - self.free_energy(visible_chain_end)
 
         parameters = [self.W, self.b_hidden, self.b_visible]
         for p in parameters:
-            updates[p] = p - T.cast(learning_rate, theano.config.floatX)
+            gradient = T.grad(cost, p, consider_constant=[visible_chain_end])
+            updates[p] = p - T.cast(learning_rate, theano.config.floatX)*gradient
+
+        if persistent_state is None:
+            monitoring_cost = mean_cross_entropy(T.nnet.sigmoid(linear_visible_activations[-1]), input)
+        else:
+            updates[persistent_state] = hidden_samples[-1]
+            monitoring_cost = self.get_pseudo_likelihood_cost(input, updates)
+
+        return monitoring_cost, updates
+
+    def get_pseudo_likelihood_cost(self, input, updates):
+        """Stochastic approximation to the pseudo-likelihood"""
+
+        # index of bit i in expression p(x_i | x_{\i})
+        bit_i_idx = theano.shared(value=0, name='bit_i_idx')
+
+        # binarize the input image by rounding to nearest integer
+        xi = T.round(input)
+        # calculate free energy for the given bit configuration
+        fe_xi = self.free_energy(xi)
+
+        # flip bit x_i of matrix xi and preserve all other bits x_{\i}
+        # Equivalent to xi[:,bit_i_idx] = 1-xi[:, bit_i_idx], but assigns
+        # the result to xi_flip, instead of working in place on xi.
+        xi_flip = T.set_subtensor(xi[:, bit_i_idx], 1 - xi[:, bit_i_idx])
+
+        # calculate free energy with bit flipped
+        fe_xi_flip = self.free_energy(xi_flip)
+
+        # equivalent to e^(-FE(x_i)) / (e^(-FE(x_i)) + e^(-FE(x_{\i})))
+        n_visible = self.b_visible.get_value(borrow=True).shape[0]
+        cost = T.mean(n_visible * T.log(T.nnet.sigmoid(fe_xi_flip -
+                                                            fe_xi)))
+
+        # increment bit_i_idx % number as part of updates
+        updates[bit_i_idx] = (bit_i_idx + 1) % n_visible
+
+        return cost
 
 
 
@@ -98,107 +148,109 @@ def run_5_rbm():
         train_set, valid_set, test_set = pickle.load(f)
 
     batch_size = 20
-    learning_rate = 0.01
-    n_epochs = 10
-    L1_reg_coeff = 0.00
-    L2_reg_coeff = 0.0001
-    n_in=28*28
+    learning_rate = 0.1
+    n_training_epochs = 1 #15
+    n_visible=28*28
     n_hidden=500
-    n_out=10
-    rng = np.random.RandomState(1234)
 
-    train_set_x, train_set_y = load_dataset(train_set)
-    valid_set_x, valid_set_y = load_dataset(valid_set)
-    test_set_x, test_set_y = load_dataset(test_set)
+    n_chains = 20
+    n_samples = 10
+    n_contrastive_divergence_steps=1
+    rng = np.random.RandomState(123)
+    theano_rng = RandomStreams(rng.randint(2 ** 30))
 
+    train_set_x, _ = load_dataset(train_set)
+    test_set_x, _ = load_dataset(test_set)
     n_train_batches = train_set_x.get_value(borrow=True).shape[0]/batch_size
 
-    # load the whole test and validation set
-    test_batch_size = valid_set_x.get_value(borrow=True).shape[0]
-    n_validation_batches = valid_set_x.get_value(borrow=True).shape[0]/test_batch_size
-    n_test_batches = test_set_x.get_value(borrow=True).shape[0]/test_batch_size
-
     x = T.matrix('x')
-    y = T.ivector('y')
 
-    hidden_layer_output, hidden_layer_params = hidden_layer(x, n_in, n_hidden, rng)
-    output_layer_output, output_layer_params = logistic_layer(hidden_layer_output, n_hidden, n_out)
+    persistent_chain = theano.shared(
+        np.zeros((batch_size, n_hidden), dtype=theano.config.floatX), borrow=True)
 
-    y_predict = T.argmax(output_layer_output, axis=1)
+    rbm = RBM(n_visible, n_hidden, rng)
 
-    # weights decay
-    L1 = abs(hidden_layer_params[0]).sum() + abs(output_layer_params[0]).sum()
-    L2 = T.sqr(hidden_layer_params[0]).sum() + T.sqr(output_layer_params[0]).sum()
-
-    cost = negative_log_likelihood_loss(output_layer_output, y) + L1_reg_coeff*L1 + L2_reg_coeff*L2
+    #persistent contrastive divergence with n_contrastive_divergence_steps steps
+    cost, updates = rbm.get_cost_updates(
+        x, learning_rate, number_of_gibbs_steps=n_contrastive_divergence_steps,
+        theano_rng=theano_rng, persistent_state=persistent_chain)
 
     minibatch_index = T.iscalar('minibatch_index')
 
-    train_model = theano.function(
+    train_rbm = theano.function(
         inputs=[minibatch_index],
-        outputs=[],
-        updates=[[p, p - learning_rate*T.grad(cost, p)]
-                 for p in (output_layer_params + hidden_layer_params)],
+        outputs=cost,
+        updates=updates,
         givens={
             x: train_set_x[minibatch_index*batch_size:(minibatch_index+1)*batch_size],
-            y: train_set_y[minibatch_index*batch_size:(minibatch_index+1)*batch_size],
-        },
-        profile=True
-    )
-
-    validation_model = theano.function(
-        inputs=[minibatch_index],
-        outputs=one_zero_loss(y_predict, y),
-        givens={
-            x: valid_set_x[minibatch_index*test_batch_size:(minibatch_index+1)*test_batch_size],
-            y: valid_set_y[minibatch_index*test_batch_size:(minibatch_index+1)*test_batch_size],
-        }
-    )
-
-    test_model = theano.function(
-        inputs=[minibatch_index],
-        outputs=one_zero_loss(y_predict, y),
-        givens={
-            x: test_set_x[minibatch_index*test_batch_size:(minibatch_index+1)*test_batch_size],
-            y: test_set_y[minibatch_index*test_batch_size:(minibatch_index+1)*test_batch_size],
         }
     )
 
     start_time = time.time()
 
-    def main_loop():
-        patience = 10000
-        patience_increase = 2
-        improvement_threshold = 0.995
-        validation_frequency = n_train_batches
-        test_score = 0.
-        best_validation_loss = np.inf
+    for epoch in range(n_training_epochs):
+        epoch_start_time = time.time()
+        costs = []
+        for batch_index in range(n_train_batches):
+            batch_start_time = time.time()
+            costs.append(train_rbm(batch_index))
+            print('Done batch %d of %d. Took %.4fs' % (batch_index, n_train_batches, time.time() - batch_start_time))
+        print('Training epoch %d of %d, cost is %f, took %.1fs' %
+              (epoch, n_training_epochs, np.mean(costs), time.time() - epoch_start_time))
+        filters = tile_raster_images(X=rbm.W.get_value(borrow=True).T, img_shape=(28, 28))
+        cv2.imshow('filter', filters)
+        cv2.waitKey(-1)
 
-        print('Going to run the training with floatX=%s' % (theano.config.floatX))
-        for epoch in range(n_epochs):
-            for minibatch_index in range(n_train_batches):
-                train_model(minibatch_index)
+    print ('Training took %d minutes' % (time.time()-start_time/60.))
 
-                iteration = epoch*n_train_batches + minibatch_index
-                if (iteration + 1) % validation_frequency == 0.:
-                    validation_cost = np.mean([validation_model(i) for i in range(n_validation_batches)])
-                    print('epoch %i, validation error %f %%' % (epoch, validation_cost * 100.))
-                    if validation_cost < best_validation_loss:
-                        if validation_cost < best_validation_loss*improvement_threshold:
-                            patience = max(patience, iteration*patience_increase)
-                        best_validation_loss = validation_cost
-                        test_score = np.mean([test_model(i) for i in range(n_test_batches)])
-                        print('  epoch %i, minibatch test error of best model %f %%' % (epoch, test_score * 100.))
-                if patience <= iteration:
-                    return epoch, best_validation_loss, test_score
-        return epoch, best_validation_loss, test_score
+    # sample from trained RBM
+    number_of_test_samples = test_set_x.get_value(borrow=True).shape[0]
+    # pick random test examples, with which to initialize the persistent chain
+    test_idx = rng.randint(number_of_test_samples - n_chains)
+    persistent_vis_chain = theano.shared(
+        np.asarray(
+            test_set_x.get_value(borrow=True)[test_idx:test_idx + n_chains],
+            dtype=theano.config.floatX
+        )
+    )
 
-    epoch, best_validation_loss, test_score = main_loop()
+    plot_every = 1000
+    (hidden_samples, hidden_activations, hidden_linear_activations,
+     visible_samples, visible_activations, linear_visible_activations), sampling_updates = theano.scan(
+        fn=lambda x: rbm.gibbs_update_visible_hidden_visible(x, theano_rng),
+        outputs_info=[None, None, None, None, None, persistent_vis_chain],
+        n_steps=plot_every
+    )
 
-    total_time = time.time()-start_time
-    print('Optimization complete in %.1fs with best validation score of %f %%, with test performance %f %%' %
-          (total_time, best_validation_loss * 100., test_score * 100.))
-    print('The code run for %d epochs, with %f epochs/sec' % (epoch, epoch/total_time))
+    sampling_updates[persistent_vis_chain] = visible_samples[-1]
+    sample_fn = theano.function(
+        [],
+        [
+            visible_activations[-1],
+            visible_samples[-1]
+        ],
+        updates=sampling_updates
+    )
+
+    image_data = np.zeros(
+        (29 * n_samples + 1, 29 * n_chains - 1),
+        dtype='uint8'
+    )
+    for idx in xrange(n_samples):
+        # generate `plot_every` intermediate samples that we discard,
+        # because successive samples in the chain are too correlated
+        vis_mf, vis_sample = sample_fn()
+        print ' ... plotting sample ', idx
+        image_data[29 * idx:29 * idx + 28, :] = tile_raster_images(
+            X=vis_mf,
+            img_shape=(28, 28),
+            tile_shape=(1, n_chains),
+            tile_spacing=(1, 1)
+        )
+
+    cv2.imshow('sampling', image_data)
+    cv2.waitKey(-1)
+
 
 
 if __name__ == "__main__":
