fa98727d7fae3cccd3e2428a167c9802488c3926
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-11-15 20:49:20 -0800

    more

--- learn_theano/deeplearning_tutorials/test_5_rbm.py
+++ learn_theano/deeplearning_tutorials/test_5_rbm.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
 
 import theano
 import theano.tensor as T
 import numpy as np
 from learn_theano.utils.download_all_datasets import get_dataset
 import cPickle
+import pickle
 import time
 
 
@@ -21,64 +23,135 @@ def load_dataset(dataset):
     return set_x, T.cast(set_y, 'int32')
 
 
-def run_0_logistic_regression():
+class RBM(object):
+    def __init__(self, n_visible, n_hidden, rng):
+        w_init = rng.uniform(
+            low=-4*np.sqrt(6./(n_visible + n_hidden)),
+            high=4*np.sqrt(6./(n_visible + n_hidden)),
+            size=(n_visible, n_hidden))
+        self.W = theano.shared(
+            np.asarray(w_init, dtype=theano.config.floatX),
+            name='W',
+            borrow=True)
+        self.b_hidden = theano.shared(
+            np.zeros((n_hidden,), dtype=theano.config.floatX),
+            name='b_hidden',
+            borrow=True
+        )
+
+        self.b_visible = theano.shared(
+            np.zeros((n_hidden,), dtype=theano.config.floatX),
+            name='b_visible',
+            borrow=True
+        )
+
+    def sample_hidden_given_visible(self, visible, theano_rng):
+        hidden_activation = T.nnet.sigmoid(T.dot(visible, self.W)+self.b_hidden)
+        hidden_sample = theano_rng.binominal(
+            size=hidden_activation.shape, n=1, p=hidden_activation, dtype=theano.config.floatX)
+        return hidden_sample
+
+    def sample_visible_give_hidden(self, hidden, theano_rng):
+        visible_activation = T.nnet.sigmoid(T.dot(hidden, self.W.T)+self.b_visible)
+        visible_sample = theano_rng.binominal(
+            size=visible_activation.shape, n=1, p=visible_activation, dtype=theano.config.floatX)
+        return visible_sample
+
+    def gibbs_update_hidden_visible_hidden(self, hidden, theano_rng):
+        visible_sample = self.sample_visible_give_hidden(hidden, theano_rng)
+        return visible_sample, self.sample_hidden_given_visible(visible_sample, theano_rng)
+
+    def gibbs_update_visible_hidden_visible(self, visible, theano_rng):
+        hidden_sample = self.sample_hidden_given_visible(visible, theano_rng)
+        return hidden_sample, self.sample_visible_give_hidden(hidden_sample, theano_rng)
+
+    def free_energy(self, visible_sample):
+        visible_bias_term = T.dot(visible_sample, self.b_visible)
+
+        wx_b = T.dot(visible_sample, self.W) + self.b_hidden
+        hidden_term = T.sum(T.log(1 + T.exp(wx_b)), axis=1)
+        return T.mean(-hidden_term - visible_bias_term)
+
+    def get_cost_updates(self, input, learning_rate, number_of_gibbs_steps, theano_rng, persistent_state):
+        if persistent_state is None:
+            chain_start = self.sample_hidden_given_visible(input, theano_rng)
+        else:
+            chain_start = persistent_state
+        (visible_samples, hidden_samples), updates = theano.scan(
+            fn=self.gibbs_update_hidden_visible_hidden,
+            outputs_info=[None, chain_start],
+            n_steps=number_of_gibbs_steps,
+            non_sequences=theano_rng
+        )
+        visible_chain_end = visible_samples[-1]
+        cost = self.free_energy(input) - self.free_energy(visible_chain_end)
+
+        parameters = [self.W, self.b_hidden, self.b_visible]
+        for p in parameters:
+            updates[p] = p - T.cast(learning_rate, theano.config.floatX)
+
+
+
+def run_5_rbm():
     mnist_pkl = get_dataset('mnist')
     with open(mnist_pkl) as f:
-        train_set, valid_set, test_set = cPickle.load(f)
+        train_set, valid_set, test_set = pickle.load(f)
 
-    batch_size = 600
-    learning_rate = 0.13
-    n_epochs = 1000
+    batch_size = 20
+    learning_rate = 0.01
+    n_epochs = 10
+    L1_reg_coeff = 0.00
+    L2_reg_coeff = 0.0001
+    n_in=28*28
+    n_hidden=500
+    n_out=10
+    rng = np.random.RandomState(1234)
 
     train_set_x, train_set_y = load_dataset(train_set)
     valid_set_x, valid_set_y = load_dataset(valid_set)
     test_set_x, test_set_y = load_dataset(test_set)
 
     n_train_batches = train_set_x.get_value(borrow=True).shape[0]/batch_size
-    n_validation_batches = valid_set_x.get_value(borrow=True).shape[0]/batch_size
-    n_test_batches = test_set_x.get_value(borrow=True).shape[0]/batch_size
+
+    # load the whole test and validation set
+    test_batch_size = valid_set_x.get_value(borrow=True).shape[0]
+    n_validation_batches = valid_set_x.get_value(borrow=True).shape[0]/test_batch_size
+    n_test_batches = test_set_x.get_value(borrow=True).shape[0]/test_batch_size
 
     x = T.matrix('x')
     y = T.ivector('y')
 
-    n_in=28*28
-    n_out=10
+    hidden_layer_output, hidden_layer_params = hidden_layer(x, n_in, n_hidden, rng)
+    output_layer_output, output_layer_params = logistic_layer(hidden_layer_output, n_hidden, n_out)
 
-    W = theano.shared(
-        np.zeros((n_in, n_out), dtype=theano.config.floatX),
-        name='W',
-        borrow=True)
-    b = theano.shared(
-        np.zeros((n_out,), dtype=theano.config.floatX),
-        name='b',
-        borrow=True
-    )
-    py_given_x = T.nnet.softmax(T.dot(x, W)+b)
-    y_predict = T.argmax(py_given_x, axis=1)
+    y_predict = T.argmax(output_layer_output, axis=1)
 
-    cost = negative_log_likelihood_loss(py_given_x, y)
+    # weights decay
+    L1 = abs(hidden_layer_params[0]).sum() + abs(output_layer_params[0]).sum()
+    L2 = T.sqr(hidden_layer_params[0]).sum() + T.sqr(output_layer_params[0]).sum()
+
+    cost = negative_log_likelihood_loss(output_layer_output, y) + L1_reg_coeff*L1 + L2_reg_coeff*L2
 
     minibatch_index = T.iscalar('minibatch_index')
 
     train_model = theano.function(
         inputs=[minibatch_index],
         outputs=[],
-        updates=(
-            [W, W - learning_rate*T.grad(cost, W)],
-            [b, b - learning_rate*T.grad(cost, b)],
-        ),
+        updates=[[p, p - learning_rate*T.grad(cost, p)]
+                 for p in (output_layer_params + hidden_layer_params)],
         givens={
             x: train_set_x[minibatch_index*batch_size:(minibatch_index+1)*batch_size],
             y: train_set_y[minibatch_index*batch_size:(minibatch_index+1)*batch_size],
-        }
+        },
+        profile=True
     )
 
     validation_model = theano.function(
         inputs=[minibatch_index],
         outputs=one_zero_loss(y_predict, y),
         givens={
-            x: valid_set_x[minibatch_index*batch_size:(minibatch_index+1)*batch_size],
-            y: valid_set_y[minibatch_index*batch_size:(minibatch_index+1)*batch_size],
+            x: valid_set_x[minibatch_index*test_batch_size:(minibatch_index+1)*test_batch_size],
+            y: valid_set_y[minibatch_index*test_batch_size:(minibatch_index+1)*test_batch_size],
         }
     )
 
@@ -86,20 +159,22 @@ def run_0_logistic_regression():
         inputs=[minibatch_index],
         outputs=one_zero_loss(y_predict, y),
         givens={
-            x: test_set_x[minibatch_index*batch_size:(minibatch_index+1)*batch_size],
-            y: test_set_y[minibatch_index*batch_size:(minibatch_index+1)*batch_size],
+            x: test_set_x[minibatch_index*test_batch_size:(minibatch_index+1)*test_batch_size],
+            y: test_set_y[minibatch_index*test_batch_size:(minibatch_index+1)*test_batch_size],
         }
     )
 
     start_time = time.time()
 
     def main_loop():
-        patience = 5000
+        patience = 10000
         patience_increase = 2
         improvement_threshold = 0.995
         validation_frequency = n_train_batches
         test_score = 0.
         best_validation_loss = np.inf
+
+        print('Going to run the training with floatX=%s' % (theano.config.floatX))
         for epoch in range(n_epochs):
             for minibatch_index in range(n_train_batches):
                 train_model(minibatch_index)
@@ -125,9 +200,6 @@ def run_0_logistic_regression():
           (total_time, best_validation_loss * 100., test_score * 100.))
     print('The code run for %d epochs, with %f epochs/sec' % (epoch, epoch/total_time))
 
-    assert(abs(best_validation_loss - 0.075) < 1e-6)
-    assert(abs(test_score == 0.07489583) < 1e-6)
-
 
 if __name__ == "__main__":
-    run_0_logistic_regression()
+    run_5_rbm()
