18a4696c5c01447bfa5762971d33f2db3b03d546
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-12-01 22:34:14 -0800

    version keras

--- use_repo.sh
+++ use_repo.sh
@@ -46,8 +46,9 @@ mkdir -p ~/.robustus_rc/wheelhouse
 # initialize robustus venv with cache path 
 robustus --cache ~/.robustus_rc/wheelhouse env $VENV
 
-# install bleeding edge theano
-pip install git+git://github.com/Theano/Theano.git
+# install bleeding edge theano at Dec, 1, 2015
+pip install git+git://github.com/Theano/Theano.git@30cc6380863b08a3a90ecbe083ddfb629a56161d
+pip install git+git://github.com/fchollet/keras.git@5956dbe8fad1642f5c6529008fd9126d920b0e76
 
 # install this folder in developer mode
 echo "Running robustus with options '$ROBUSTUS_OPTIONS'"
