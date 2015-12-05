73a068227d9d64cb42d0765a7ff76dea9202623b
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-12-04 16:38:39 -0800

    run with another float

--- learn_theano/pylearn2/test_pylearn_tutorials.py
+++ learn_theano/pylearn2/test_pylearn_tutorials.py
@@ -1,6 +1,7 @@
 from learn_theano.pylearn2.custom_autoencoder_2 import custom_autoencoder_run
 from learn_theano.pylearn2.custom_logreg_1 import custom_log_reg_run
 from learn_theano.pylearn2.standard_mlp_0 import standard_mlp_run
+import theano
 
 
 def test_standard_mlp():
@@ -8,7 +9,10 @@ def test_standard_mlp():
 
 
 def test_custom_log_reg():
+    old_floatx = theano.config.floatX
+    theano.config.floatX = 'float64'
     custom_log_reg_run(1)
+    theano.config.floatX = old_floatx
 
 
 def test_custom_autoencoder():
